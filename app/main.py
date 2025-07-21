eea1def18a4c9ad4b38aed6b1f69ba97f9abf5db
chaggard001 <c.stephen.haggard@gmail.com>
2025-07-21 16:09:47 -0400

    fixed origins

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ origins = [
     'http://localhost:5500',
     'http://localhost:8000',
     'http://127.0.0.1:5500',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'https://fus-elearning.org',
+    'https://www.fus-elearning.org'
 ]
 
 app.add_middleware(
